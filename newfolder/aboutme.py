25fd983b649cd4482e5c0ee408eb65adf367633c
LailaAlriyami <70027924+LailaAlriyami@users.noreply.github.com>
2020-08-21 23:42:42 +0400

    adding file py

---
+++ newfolder/aboutme.py
@@ -0,0 +1,165 @@
+{
+  "Clubs": [],
+  "Consents": [
+    {
+      "SignedBy": {
+        "Value": "06505817"
+      },
+      "SignedOn": "2018-10-16T06:55:47.633",
+      "Consented": true
+    },
+    {
+      "SignedBy": {
+        "Value": "06505817"
+      },
+      "SignedOn": "2018-10-16T06:55:47.633",
+      "Consented": true
+    },
+    {
+      "SignedBy": {
+        "Value": "06505817"
+      },
+      "SignedOn": "2018-10-16T06:55:47.633",
+      "Consented": true
+    },
+    {
+      "SignedBy": {
+        "Value": "06505817"
+      },
+      "SignedOn": "2018-10-16T06:55:47.633",
+      "Consented": true
+    },
+    {
+      "SignedBy": {
+        "Value": "06505817"
+      },
+      "SignedOn": null,
+      "Consented": true
+    }
+  ],
+  "ContactInfo": {
+    "GivenName": "Laila",
+    "MiddleName": "Majid Mohammed",
+    "NameSuffix": null,
+    "Surname": "Al Riyami",
+    "Address": {
+      "Address": {
+        "Id": {
+          "Value": 5359191
+        },
+        "Street": "PO Box 851",
+        "AdditionalLines": [],
+        "City": "Muscat",
+        "County": null,
+        "PrimaryRegion": null,
+        "PrimaryRegionDescription": null,
+        "Country": {
+          "Value": "OMN"
+        },
+        "PostalCode": "100",
+        "Coordinates": null,
+        "TimeZone": null,
+        "ValidationDetails": null
+      },
+      "Company": "C/o Saleh Abdullah Al Khamyasi",
+      "AddressedTo": "Laila MajidMohammed Al Riyami",
+      "AttentionLine": null
+    },
+    "DigitalContacts": [
+      {
+        "RawText": "l.al-riyami@hotmail.com"
+      }
+    ],
+    "DisplayName": "Laila Majid Mohammed Al Riyami",
+    "PhoneNumbers": [
+      {
+        "Type": 1,
+        "RawText": "96897733835",
+        "RegionCode": {
+          "Value": "OM"
+        }
+      },
+      {
+        "Type": 2,
+        "RawText": "96897733835",
+        "RegionCode": {
+          "Value": "OM"
+        }
+      },
+      {
+        "Type": 4,
+        "RawText": "96897733835",
+        "RegionCode": {
+          "Value": "OM"
+        }
+      },
+      {
+        "Type": 3,
+        "RawText": "96897733835",
+        "RegionCode": {
+          "Value": "OM"
+        }
+      },
+      {
+        "Type": 0,
+        "RawText": "96897733835",
+        "RegionCode": {
+          "Value": "OM"
+        }
+      }
+    ]
+  },
+  "CurrentLoginInformation": {
+    "Email": {
+      "RawText": "l.al-riyami@hotmail.com"
+    },
+    "ID": {
+      "Value": "06505817"
+    },
+    "UserName": "l.al-riyami@hotmail.com"
+  },
+  "Education": [
+    {
+      "Complete": false,
+      "Language": {
+        "Language": {
+          "Value": "ar-SA"
+        },
+        "Type": 0
+      },
+      "LevelAwards": [],
+      "Medium": 1,
+      "Path": {
+        "Value": 4
+      }
+    }
+  ],
+  "Id": {
+    "Value": "06505817"
+  },
+  "Identity": {
+    "NamePrefix": null,
+    "GivenName": "Laila",
+    "MiddleName": "Majid Mohammed",
+    "Surname": "Al Riyami",
+    "NameSuffix": null,
+    "Gender": {
+      "Value": "FEMALE"
+    },
+    "Credentials": null
+  },
+  "MemberSince": "2017-04-01T00:00:00",
+  "Signees": {
+    "06505817": {
+      "NamePrefix": null,
+      "GivenName": "Laila",
+      "MiddleName": "Majid Mohammed",
+      "Surname": "Al Riyami",
+      "NameSuffix": null,
+      "Gender": {
+        "Value": "FEMALE"
+      },
+      "Credentials": null
+    }
+  }
+}
